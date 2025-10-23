eaace372305f430e36fe81841c69383f0536800c
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-10-23 13:29:21 -0400

    half23

--- main.ts
+++ main.ts
@@ -5,4 +5,25 @@
  * This program ...
 */
 
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+let currentDistance: Number = 0
+let neopixelstrip : neopixel.Strip = null
+
+input.onButtonPressed(Button.A, function()
+{
+    currentDistance = sonar.ping(
+        DigitalPin.P1,
+        DigitalPin.P2,
+        PingUnit.Centimeters
+    )
+    if (currentDistance < 10)
+    neopixelstrip
+    
+
+})
+
+
 
