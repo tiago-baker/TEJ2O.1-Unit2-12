645a17666eaa829854d00c27ed4add6f3f38f2ec
tiago-baker <tiago.baker@stu.ocsb.ca>
2025-11-05 20:59:26 -0500

    done code from line 5 to 53

--- main.ts
+++ main.ts
@@ -2,28 +2,52 @@
  *
  * Created by: Tiago Baker
  * Created on: Oct 2025
- * This program ...
+ * This program turns neopixels red if within 10cm else turn green if more than 10cm
 */
 
 //setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
-let currentDistance: Number = 0
-let neopixelstrip : neopixel.Strip = null
+// variables
+let distance = 0
+let neopixelStrip = neopixel.create(DigitalPin.P16, 10, NeoPixelMode.RGB)
 
-input.onButtonPressed(Button.A, function()
-{
-    currentDistance = sonar.ping(
+// clear neopixels
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+// When button A is pressed
+input.onButtonPressed(Button.A, function () {
+    // Read distance from the sonar in cm
+    distance = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
         PingUnit.Centimeters
     )
-    if (currentDistance < 10)
-    neopixelstrip
-    
 
+    // If distance < 10 cm, turn all neopixels red
+    if (distance < 10) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Happy)
+    }
+    // else (distance >= 10 cm) turn them green
+    else { (distance >= 10) 
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.show()
+        basic.showIcon(IconNames.Happy)
+    }
 })
-
-
-
